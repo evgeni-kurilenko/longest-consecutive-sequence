39439ee3577239cfc2da86b714832e88ed5059c3
Evgeni Kurilenko <evgeni.kurilenko@gmail.com>
2018-03-11 20:59:50 +0300

    Sequence sol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    // your solution here
+    function compareNumbers(a, b) {
+        return a - b;
+    }
+
+    var ll = [];
+    var max = 0, l=0;
+    array.sort(compareNumbers);
+
+    if (array.length > 1) {
+        max = 1;
+        l = 1;
+        for (var i = 0; i < array.length - 1; i++) {
+            if (array[i]==array[i+1] - 1) {
+                l++;
+            } else if (array[i+1]-array[i]>1) {
+                l=1;
+            }
+            ll.push(l);
+
+            //console.log("l = "+ "" + l );
+        }
+
+        ll.sort(compareNumbers);
+        max = ll.pop();
+
+        ///console.log(ll);
+    } else if (array.length == 0) {
+        max = 0;
+    } else if (array.length == 1) {
+        max = 1;
+    }
+
+    return max;
 }
